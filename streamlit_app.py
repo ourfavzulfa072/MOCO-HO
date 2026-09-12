524a4e532f36e17449de257b1fe2f5e49de9dd02
zulfa <108939822+ourfavzulfa072@users.noreply.github.com>
2026-09-12 10:43:52 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import pandas as pd
 # -----------------------------------------------------------------------------
 st.set_page_config(
     page_title="MOCO - Audit Control Room",
-    page_icon="⛏️",
+    page_icon="BIMA NUSA INT",
     layout="wide"
 )
 
